c26d732a4b8756ca84e716271489971680975929
cihan <b.cihancengiz@gmail.com>
2020-07-16 10:05:32 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/kareagency/reff/reff/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kareagency.reff.reff
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'r.e.f.f.',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData.dark(),
+      home: Text('reff'),
+    );
+  }
+}
